c86614e5b04cece96987c6d304333e80b650a508
Andrew Godfrey <andrew@gfreys.com>
2026-04-03 20:06:15 -0700

    add simple tool `Get-FullName` (alias: `gfn`)

--- lib/inst/contextTool_prefs.ps1
+++ lib/inst/contextTool_prefs.ps1
@@ -3,6 +3,7 @@ param($installationTracker)
 $stage = $installationTracker.StartStage("contextTool")
 
 Install-InteractiveAlias $stage 'pl' 'Launch-Plan'
+Install-InteractiveAlias $stage 'gfn' 'Get-FullName'
 Install-Folder $stage "$home\prat\auto\context"
 Install-Folder $stage "$home\prat\auto\context\running"
 
---
+++ pathbin/Get-FullName.ps1
@@ -0,0 +1,2 @@
+param($path)
+(dir $path).FullName
