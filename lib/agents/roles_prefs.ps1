49aa6620cbc12d6f3ca4980595bf0aa86d470072
Andrew Godfrey <andrew@gfreys.com>
2026-06-21 14:39:23 -0700

    add skillGroups (for agent-role support)

---
+++ lib/agents/roles_prefs.ps1
@@ -0,0 +1,7 @@
+# Skill groups contributed by the prefs layer. See Get-AgentRoles for how layers compose into roles.
+@{
+    skillGroups = @{
+        writing       = @('kql-techniques')
+        planLifecycle = @('end-plan', 'wrap-pl')
+    }
+}
