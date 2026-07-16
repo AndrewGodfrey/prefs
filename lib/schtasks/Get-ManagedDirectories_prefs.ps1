688bc266e5a8c37576174ddeb22da08743661b66
Andrew Godfrey <andrew@gfreys.com>
2026-07-15 19:29:17 -0700

    Get-ManagedDirectories: move the 'agent temp' case from prefs to prat

--- lib/schtasks/Get-ManagedDirectories_prefs.ps1
+++ lib/schtasks/Get-ManagedDirectories_prefs.ps1
@@ -2,8 +2,3 @@
 param([switch] $AddRecommendedDirectories = $false, [string] $UserName = $env:USERNAME)
 
 . $home\prat\lib\schtasks\Get-ManagedDirectories_prat.ps1 -AddRecommendedDirectories
-
-$agentTemp = "C:\Users\${UserName}_agent\AppData\Local\Temp"
-if (Test-Path $agentTemp) {
-    @{ path = $agentTemp; days = 14 }
-}
